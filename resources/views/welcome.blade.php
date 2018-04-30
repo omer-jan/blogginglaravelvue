134d23ac54379196a7d559cd4518f41c0d267254
omer-jan <paghmane@yahoo.com>
2018-04-30 14:31:34 +0430

    step one

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -73,6 +73,7 @@
                     @else
                         <a href="{{ route('login') }}">Login</a>
                         <a href="{{ route('register') }}">Register</a>
+                        <h1>sfsdfsd</h1>
                     @endauth
                 </div>
             @endif
